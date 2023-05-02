782050678432c6622a975ff10c6e5dda033cc8a5
Ddavila09 <ddavilax23@gmail.com>
2023-05-01 19:19:05 -0500

    Destruct

---
+++ predict_destruct.js
@@ -0,0 +1,67 @@
+
+
+//1-
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+//output would be 
+// logs 'tesla'
+// logs 'mercedes'
+
+//2-
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+//it will give an error. Name not defined
+
+//3-
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+//logs 12345
+//logs undefined because person doesn't have password
+
+//4-
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+//logs false because 2 and 5 are not equal
+// logs true because 2 and 2 are equal
+
+//5- 
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+// logs value
+// logs [1, 5, 1, 8, 3, 3]
+// logs 1
+// logs 5 because its assigned the second value in second key.
